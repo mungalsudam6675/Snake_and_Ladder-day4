12c4a8f3703e31c307d1d9c42ef5f7b14937cc67
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-01-30 03:52:04 +0530

    Adding Uc2 To print dice valueUc1.java

--- Uc1.java
+++ Uc1.java
@@ -2,5 +2,7 @@ class Uc1{
 	public static void main(String s[]){
 		int start = 0;
 		System.out.println("Position = "+start);
+		int check=(int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Dice giver "+check);
 	}
 }
