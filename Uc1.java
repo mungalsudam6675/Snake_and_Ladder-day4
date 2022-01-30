a71a945492cf5ebee2f4aa43b4d4e5079e387d00
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-01-30 13:51:20 +0530

    Adding Additional logic of snake and ladder game

--- Uc1.java
+++ Uc1.java
@@ -1,8 +1,21 @@
 class Uc1{
 	public static void main(String s[]){
-		int start = 0;
-		System.out.println("Position = "+start);
-		int check=(int)Math.floor(Math.random()*10)%6+1;
-		System.out.println("Dice giver "+check);
+		int position = 0;
+		System.out.println("Position = "+position);
+		int dice=(int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Dice giver "+dice);
+		double empCheck= Math.floor(Math.random()* 10) % 3;
+		System.out.println("Check = "+empCheck);
+		if(empCheck==1){
+			position=position;
+		}
+		else if(empCheck==2){
+			position=position+dice;
+		}
+		else{
+			position=position;
+			System.out.println("Check is 0 so no play");
+		}
+		System.out.println("Position of player is "+position);
 	}
 }
